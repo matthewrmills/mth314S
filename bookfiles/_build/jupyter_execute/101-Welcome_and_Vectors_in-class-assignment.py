8ad4de7cfc3c8377b2e2bdae0a15314488285b7c
Matt Mills <mattmills92@gmail.com>
2021-05-24 09:58:25 -0400

    Assignment 104 done.

--- bookfiles/_build/jupyter_execute/101-Welcome_and_Vectors_in-class-assignment.py
+++ bookfiles/_build/jupyter_execute/101-Welcome_and_Vectors_in-class-assignment.py
@@ -601,5 +601,7 @@ YouTubeVideo("fNk_zzaMoSs",width=640,height=360, cc_load_policy=True)
 # 102-Matrix_Mechanics_in-class-assignment.ipynb
 # 103--Linear_Equations_pre-class-assignment.ipynb
 # 103-Linear_Equations_in-class-assignment.ipynb
+# 104--Affine_Transformations_Robotics_pre-class-assignment.ipynb
+# 104-Affine_Transformations_Robotics_in-class-assignment.ipynb
 # ```
 # 
